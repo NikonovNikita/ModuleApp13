f6bde47e8ee620b06a2c3391d73b4fd6d9a00d39
Nikita Nikita <jassie.jassie@yandex.ru>
2024-03-13 14:57:46 +0300

    Итоговые задания 13 модуля

--- ModuleApp13/Program.cs
+++ ModuleApp13/Program.cs
@@ -1,7 +1,48 @@
-﻿class Program
+﻿namespace ModuleApp13;
+class Program
 {
     static void Main(string[] args)
     {
+        string text;
+        
+        using (StreamReader sr = new StreamReader(@"C:\Users\Никонов\OneDrive\Рабочий стол\Obl.txt"))
+        {
+            text = sr.ReadToEnd();
+        }
 
+        var noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+        string[] splitedText = GetSplitedText(noPunctuationText);
+        
+        Console.WriteLine($"Кол-во слов в тексте: {splitedText.Length}\n");
+
+        Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+        for(int i = 0; i < splitedText.Length; i++)
+        {
+            if (!dictionary.ContainsKey(splitedText[i]))
+            {
+                dictionary.TryAdd(splitedText[i], 1);
+                continue;
+            }
+            dictionary[splitedText[i]]++;
+        }
+
+        List<KeyValuePair<string, int>> list = dictionary.ToList();
+        list.Sort((x, y) => x.Value.CompareTo(y.Value));
+
+        var FrequentlyUsedWords = list.GetRange(list.Count - 10, 10);
+        
+        Console.WriteLine($"Самые часто используемые слова в тексте:\n");
+        
+        foreach (var item in  FrequentlyUsedWords)
+            Console.WriteLine($"{item.Key} --> {item.Value}");
+    }
+
+    static string[] GetSplitedText(string text)
+    {
+        char[] separators = { ' ', '\n', '\r' };
+        string[] splitedText = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+        return splitedText;
     }
 }
---
+++ Task13.6.1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Diagnostics;
+
+namespace ModuleApp13;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] splitedText = GetSplitedText();
+
+        Console.WriteLine($"Время вставки в List<T>: {TestingList(new Stopwatch(), splitedText)} ms");
+        Console.WriteLine($"Время вставки в LinkedList<T>: {TestingLinkedList(new Stopwatch(), splitedText)} ms");
+    }
+
+    static string[] GetSplitedText()
+    {
+        string text;
+        char[] separators = { ' ', '\n', '\r' };
+
+        using (StreamReader sr = new StreamReader(@"C:\Users\Никонов\OneDrive\Рабочий стол\Obl.txt"))
+        {
+            text = sr.ReadToEnd();
+        }
+
+        var noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+
+        return noPunctuationText.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+    }
+
+    static long TestingList(Stopwatch stopwatch, string[] array)
+    {
+        var list = new List<string>(array);
+
+        stopwatch.Start();
+
+        list.InsertRange(list.Count / 2, array);
+
+        stopwatch.Stop();
+
+        Console.WriteLine($"Кол-во элементов после вставки в List<T>: {list.Count}");
+
+        return stopwatch.ElapsedMilliseconds;
+    }
+
+    static long TestingLinkedList(Stopwatch stopwatch, string[] array)
+    {
+        var linkedList = new LinkedList<string>(array);
+        LinkedListNode<string> node = linkedList.Find("Обломовщина");
+
+        stopwatch.Start();
+
+        for(int i = 0; i < array.Length; i++)
+        {
+            linkedList.AddAfter(node, array[i]);
+            node = node.Next;
+        }
+
+        stopwatch.Stop();
+
+        Console.WriteLine($"Кол-во элементов после вставки в LinkedList<T>: {linkedList.Count}");
+
+        return stopwatch.ElapsedMilliseconds;
+    }
+}
